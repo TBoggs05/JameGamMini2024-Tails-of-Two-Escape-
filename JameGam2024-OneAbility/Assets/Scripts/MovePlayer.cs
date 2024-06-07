a3e675e62403c6ee2883e56434e548d114bac4c4
TBoggs05 <Trace.Boggs05@gmail.com>
2024-06-07 13:42:18 -0500

    fixed jump mechanic

--- JameGam2024-OneAbility/Assets/Scripts/MovePlayer.cs
+++ JameGam2024-OneAbility/Assets/Scripts/MovePlayer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,28 +7,77 @@ public class MovePlayer : MonoBehaviour
 {
 
     [SerializeField] private float speed;
-    [SerializeField] private Vector2 jumpHeight;
-    [SerializeField] private bool InAir = false;
-    private Rigidbody2D rb2d;
+    [SerializeField] private float jumpTime;
+    private Rigidbody2D rbody;
 
+    private float jumpPressure;
+    private float minJump;
+    private float maxJumpPressure;
+    public Vector2 boxSize;
+    public float castDistance;
+    public LayerMask groundLayer;
     void Start()
     {
-        jumpHeight = new Vector2(0, 3);
         speed = 5f;
-        rb2d = GetComponent<Rigidbody2D>();
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        //ADD CHECK HERE FOR IF COLLIDING WITH SOMETHING WITH "GROUND" TAG TO AVOID WALL JUMPING!!!
-        InAir = false;
-        Debug.Log("InAir false");
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        InAir = true;
-        Debug.Log("InAir True");
+        rbody = GetComponent<Rigidbody2D>();
+        maxJumpPressure = 10f;
     }
     void Update()
+    {
+        checkMove();
+        //jumping code
+        if (checkGround())
+        {
+            //holding jump butotn
+            if (Input.GetKey(KeyCode.W))
+            {
+                if (jumpPressure < maxJumpPressure)
+                {
+                    jumpPressure += Time.deltaTime * 10f;
+                }
+                
+                else
+                {
+                    jumpPressure = maxJumpPressure;
+                }
+            }
+            //not holding jump button
+            else
+            {
+                //jump since no longer holding button and jump pressure exists
+                if(jumpPressure > 0f)
+                {
+                    Debug.Log(jumpPressure);
+                    if(jumpPressure < 2.5f)
+                    {
+                        jumpPressure = 2.5f;
+                    }
+                    jumpPressure = jumpPressure + minJump;
+                    rbody.velocity = new Vector3(0f,jumpPressure,0f);
+                    jumpPressure = 0f;
+                    
+                }
+            }
+
+        }
+
+
+    }
+    //checks ground for jumping
+    bool checkGround()
+    {
+        if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
+            return true;
+        else
+            return false;
+    }
+    //draws jumping hitbox
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position - transform.up * castDistance, boxSize);
+    }
+    //moving code
+    void checkMove()
     {
         //Move left
         if (Input.GetKey(KeyCode.A))
@@ -39,47 +89,8 @@ public class MovePlayer : MonoBehaviour
         {
             gameObject.transform.Translate(Vector3.right * Time.deltaTime * speed);
         }
-        //JumpCode
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            StartCoroutine(jumpCoroutine());
-            
-        }
-
-
     }
-    IEnumerator jumpCoroutine()
-    {
-        //do nothing until we are confirmed on ground.
-        while (InAir) { yield return null; }
-        jumpHeight = new Vector2(0, 3);
-        float jumpTime = Time.time;
-        while(!(Input.GetKeyUp(KeyCode.W) && !InAir))
-        {
-            //crouch down to jump animation?
-            yield return null;
-        }
-        jumpTime = Time.time - jumpTime;
-        Debug.Log(jumpTime);
-             //on release, jump based on time.
-             //max case
-            if(jumpTime >= 1f)
-            {
-            jumpHeight.y *= 3f;
-            }
-            //min case
-            else if(jumpTime < 0.5f)
-            {
-            jumpHeight.y *= 1f;
-            }
-            //standard case
-            else
-            {
-            jumpHeight.y *= jumpTime * 2;
-            }
-
-            //jump force code
-            rb2d.AddForce(jumpHeight, ForceMode2D.Impulse);
-    }
-
+  
+   
+  
 }
