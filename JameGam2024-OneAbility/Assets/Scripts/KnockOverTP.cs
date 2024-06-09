c270847b05f836ea66c55d47f1d3614f25045e36
Frickinstein1 <goldcrusher77@gmail.com>
2024-06-09 00:00:21 -0500

    BALLS

---
+++ JameGam2024-OneAbility/Assets/Scripts/KnockOverTP.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockOverTP : MonoBehaviour
+{
+    public Animator knockOverAnimation;
+    private Rigidbody2D mommaRbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        //if momma collided
+        if(collider.gameObject.name == "MommaPlayer" || collider.gameObject.name == "MommaPlayer(Clone)")
+        {
+            mommaRbody = collider.gameObject.GetComponent<Rigidbody2D>();
+            //if force on rigidbody is high enough(bash)
+            if(mommaRbody.velocity.x > 1f)
+            {
+            //Trigger the animation
+            Debug.Log("KNOCK OVER");
+                knockOverAnimation.SetTrigger("KnockOver");
+                GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+                GetComponent<BoxCollider2D>().enabled = false;
+
+            }
+
+        }
+    }
+
+
+
+}
