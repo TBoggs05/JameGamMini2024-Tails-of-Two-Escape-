0313f16a7f9ffe35a5e3b73ca37555f5a789281a
Frickinstein1 <goldcrusher77@gmail.com>
2024-06-09 00:16:00 -0500

    UP 2 Date

--- JameGam2024-OneAbility/Assets/Scripts/SwitchLevel.cs
+++ JameGam2024-OneAbility/Assets/Scripts/SwitchLevel.cs
@@ -9,23 +9,27 @@ public class SwitchLevel : MonoBehaviour
     private string sceneName;
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if(other.gameObject.CompareTag("Player"))
+        {
         currentScene = SceneManager.GetActiveScene();
         sceneName = currentScene.name;
-       
-        switch (sceneName)
-        {
-            case "StartScreen":
-                LoaderScript.LoadLevel("Bedroom");
-                break;
-            case "Bedroom":
-                LoaderScript.LoadLevel("Bathroom");
-                break;
-            case "Bathroom":
-                LoaderScript.LoadLevel("Attic");
-                break;
-            case "Attic":
-                LoaderScript.LoadLevel("EndScreen");
-                break;
+
+            switch (sceneName)
+
+            {
+                case "StartScreen":
+                    LoaderScript.LoadLevel("Bedroom");
+                    break;
+                case "Bedroom":
+                    LoaderScript.LoadLevel("Bathroom");
+                    break;
+                case "Bathroom":
+                    LoaderScript.LoadLevel("Attic");
+                    break;
+                case "Attic":
+                    LoaderScript.LoadLevel("EndScreen");
+                    break;
+            }
         }
         
     }
